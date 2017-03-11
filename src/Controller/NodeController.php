1d9ce8ee8378df0aac9cf8596d4184ce5a936332
Dominic Scheirlinck <dominic@varspool.com>
2017-03-11 14:42:06 +0000

    Show node info

--- src/Controller/NodeController.php
+++ src/Controller/NodeController.php
@@ -16,6 +16,8 @@ class NodeController extends BaseController
 
     public function showAction(string $id, ?string $prefix, Request $request)
     {
+        $info = $this->disque->info();
+
         if ($prefix !== substr($id, 0, 8)) {
             return $this->redirect($this->url->generate('disque_admin_node_show', [
                 'id' => $id,
@@ -23,15 +25,25 @@ class NodeController extends BaseController
             ]), 302);
         }
 
-        $info = $this->disque->info();
-
         return $this->render('node/show.html.twig', [
             'prefix' => $prefix,
-            'id' => $id,
             'info' => $info,
+            'id' => $id,
         ]);
     }
 
+    /**
+     * A simple table of HELLO details
+     *
+     * Shows ID, hostname, port and priority
+     *
+     * @param null|string $prefix
+     * @param Request     $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
     public function tableComponent(?string $prefix, Request $request)
     {
         $node = $this->disque->getConnectionManager()->getCurrentNode();
@@ -44,6 +56,27 @@ class NodeController extends BaseController
         return $this->render('node/_table.html.twig', [
             'hello' => $hello,
             'prefix' => $prefix
+        ]);
+    }
+
+    /**
+     * A table of the INFO details for a given connection
+     *
+     * Note: we don't yet use a factory for connections, just a single connection per page. So, we need the prefix
+     * to match the ID here, for the middleware to stay simple.
+     *
+     * @param string      $id
+     * @param null|string $prefix
+     * @param Request     $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public function infoTableComponent(array $stats, Request $request)
+    {
+        return $this->render('node/_infoTable.html.twig', [
+            'stats' => $stats,
         ]);
     }
 }
