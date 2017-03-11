00413dd7a0ab357700d57e4abd3d3dec73fd3f3a
Dominic Scheirlinck <dominic@varspool.com>
2017-03-11 06:42:21 +0000

    Added node information to overview

--- src/Controller/OverviewController.php
+++ src/Controller/OverviewController.php
@@ -2,6 +2,7 @@
 
 namespace Varspool\DisqueAdmin\Controller;
 
+use Disque\Connection\Node\Node;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -10,6 +11,19 @@ class OverviewController extends BaseController
     public function indexAction(Request $request)
     {
         return $this->render('overview/index.html.twig');
+    }
+
+    public function nodesComponent(Request $request)
+    {
+        $node = $this->disque->getConnectionManager()->getCurrentNode();
+        /**
+         * @var Node $node
+         */
+        $hello = $node->getHello();
+
+        return $this->render('overview/_nodes.html.twig', [
+            'hello' => $hello,
+        ]);
     }
 
     public function navComponent(string $route, Request $request)
